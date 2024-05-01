fab30607d6292f6786ef8e7fc02e7a4f57a4aa9b
Tania Bezerra <nybrasil@gmail.com>
2024-04-30 20:08:34 -0400

    created js files

---
+++ Assets/Scripts/main.js
@@ -0,0 +1,46 @@
+// Feature: Display User's Name in Heading 1
+    var header = document.querySelector('h1');
+    var userInput = document.getElementById('userInput');
+    var workoutPage = document.getElementById('Workout-Page');
+    var welcomeMessage = document.getElementById('Welcome');
+    var previousArrow = document.getElementById("cal-prev");
+    var nextArrow = document.getElementById("cal-next");
+    var userName = localStorage.getItem('userName'); // Check if User Name is Already Stored in Local Storage
+
+    // Function: Retrieve User Name if it's Been Submitted Before
+    if (userName) {
+        header.textContent = `${userName}'s Workout Tracker`; // If User Name is Already Stored, Update Header With the Stored Name
+        workoutPage.style.display = 'block'; // Show the Workout Page Divider
+        userInput.style.display = 'none'; // Hide the User Name Form
+        welcomeMessage.style.display = 'block'; // Show the Welcome Message
+
+        // Function: Submit User Name For the First Time
+    } else {
+        userInput.addEventListener('submit', function (event) { // Add Event Listener to Form
+            event.preventDefault(); // Prevent Form Submission
+            var userName = document.getElementById('User-Name').value; // Retrieve Input Value From Form Field
+            header.textContent = `${userName}'s Workout Tracker`; // Update Text Content of Header with the User's Name
+            localStorage.setItem('userName', userName); // Save the User's Name to Local Storage
+            workoutPage.style.display = 'block'; // Show the Workout Page Divider
+            userInput.style.display = 'none'; // Hide User Name Form Once Submitted
+            welcomeMessage.style.display = 'none'; // Hide the Welcome Message
+        });
+    }
+
+// Feature: Display Random Quote (Quotable API)
+$.ajax({
+    url: 'https://api.quotable.io/random',
+    dataType: 'json',
+    success: function (data) {
+        var quote = data.content;
+        $('#Quote').text('"' + quote + '"');
+    },
+    error: function () {
+        $('#Quote').text("Your self-worth is determined by you. You don't have to depend on someone telling you who you are.");
+    }
+});
+
+// Feature: Display Date & Time
+var currentDate = dayjs().format('dddd, MMMM D, YYYY, h:mm a');
+$('#currentDate').html(currentDate); // Sets Date & Time In Dashboard Header Area
+
