06e756467ae9600c4e2329cb6fb827b25fe2185b
Emily Simone <109311858+emsim11@users.noreply.github.com>
2024-05-09 20:35:11 -0400

    Fix Connection.js File

--- config/connection.js
+++ config/connection.js
@@ -15,15 +15,4 @@ if (process.env.DB_URL) {
   );
 };
 
-const connectToDatabase = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('Connection to the database has been established successfully');
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-connectToDatabase();
-
 module.exports = sequelize;
